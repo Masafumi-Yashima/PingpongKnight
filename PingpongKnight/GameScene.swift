ce7f3a58b821ddae2a33873b28bfe6fd84c2beb2
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-05-09 17:36:45 +0900

    初期設定完了

---
+++ PingpongKnight/GameScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameScene.swift
+//  PingpongKnight
+//
+//  Created by YashimaMasafumi on 2021/05/09.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
--- PingpongKnight/ViewController.swift
+++ PingpongKnight/ViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        let scene = GameScene(size: skView.frame.size)
+        skView.presentScene(scene)
         // Do any additional setup after loading the view.
     }
 
